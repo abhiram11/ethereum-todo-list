5063b862c0558c13cc99722cc41ea9ce1b5b06d1
Abhiram Satpute <abhiram.satpute@gmail.com>
2021-04-13 08:03:13 +0530

    added index.html to the project

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,34 @@ pragma solidity >=0.5.0;
 contract ToDoList {
     // keep track of number of tasks
     uint public taskCount = 0; // state variable
+
+    struct Task{
+        uint id; 
+        string content;
+        bool completed; // if task done or not
+    }
+
+    //how to access storage on blockchain
+
+    //state variable
+    mapping(uint => Task) public tasks; //mapping stores as key-value pair, key = uint
+
+    //add some "content" so that a few things already present before customer sees the deployment page.
+
+    constructor() public {
+        createTask("Check out this trial list by Abhiram!");
+    }
+
+
+    function createTask(string memory _content) public {
+        // write some code to add task ON mapping
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false); //since new task, so set completed = false
+    }
+
+    
+
+
+
+
 }
