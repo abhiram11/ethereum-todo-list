9b22b66042f3148765a3ab01cfb246eaf31016e9
Abhiram Satpute <abhiram.satpute@gmail.com>
2021-04-15 18:33:33 +0530

    added client app to blockchain successfully via metamask and web3, had issues since web3 is deprecated but still worked around

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@
 // Connect Browser to Blockchain -> MetaMask
 // Connect Client side application to the Blockchain -> Web3.js
 
-
 App = {
+    // contracts: {},
+
     load: async () => {
         // load app...
         console.log("App loading.....!!!!")
         await App.loadWeb3() // CONNECT TO BLOCKCHAIN
+        await App.loadAccount()
+        await App.loadContract()
+
     },
 
     loadWeb3: async() => {
@@ -19,36 +23,49 @@ App = {
         }
 
         //talk to blockchain by metamask extension with web3.js. the configuration that they suggest is:
-        window.addEventListener('load', async () => {
 
-            // Modern dapp browsers...
-            if (window.ethereum) {
-                window.web3 = new Web3(ethereum);
+        // Modern dapp browsers...
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum);
 
-                try {
-                    // Request account access if needed
-                    await ethereum.enable();
+            try {
+                // Request account access if needed
+                await ethereum.enable();
 
-                    // Accounts now exposed
-                    web3.eth.sendTransaction({/* ... */});
-                } catch (error) {
-                    // User denied account access...
-                }
-
+                // Accounts now exposed
+                web3.eth.sendTransaction({/* ... */});
+            } catch (error) {
+                // User denied account access...
             }
 
-            else if (window.web3) {
-                window.web3 = new Web3(web3.currentProvider);
+        }
 
-                // Accounts always exposed
-            web3.eth.sendTransaction({/* ... */});
-            }
+        else if (window.web3) {
+            window.web3 = new Web3(web3.currentProvider);
 
-            // Non-dapp browsers...
-            else{
-                console.log('Non-Ethereum browser detected. You should consider MetaMask!!')
-            }
-        }); 
+            // Accounts always exposed
+        web3.eth.sendTransaction({/* ... */});
+        }
+
+        // Non-dapp browsers...
+        else{
+            console.log('Non-Ethereum browser detected. You should consider MetaMask!!')
+        }
+    },
+
+    loadAccount: async () => {
+        App.account = web3.eth.accounts[0] //eth object will hold all accounts, eth created/loaded by web3
+        console.log(App.account)
+    },
+
+    loadContract: async () => {
+
+        // Create javascript version of smart contract
+        const todoList = await $.getJSON('ToDoList.json')
+        App.contracts.ToDoList = TruffleContract(todoList)
+        App.contracts.ToDoList.setProvider(App.web3Provider)
+        
+        console.log(todoList)
 
 
     }
@@ -61,3 +78,81 @@ $(() => {
     })
 
 })
+
+
+
+/*
+
+
+// Connect Browser to Blockchain -> MetaMask
+// Connect Client side application to the Blockchain -> Web3.js
+import detectEthereumProvider from '@metamask/detect-provider';
+
+App = {
+    load: async () => {
+        // load app...
+        console.log("App loading.....!!!!")
+        await App.loadWeb3() // CONNECT TO BLOCKCHAIN
+        await App.loadAccount()
+    },
+
+    loadWeb3: async() => {
+        
+        const provider = await detectEthereumProvider()
+
+        if (provider){
+            App.web3Provider = provider
+            web3 = new Web3(provider)
+        } else {
+            window.alert("Please connect to Metamask..!!")
+        }
+
+        //talk to blockchain by metamask extension with web3.js. the configuration that they suggest is:
+
+        // Modern dapp browsers...
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum);
+
+            try {
+                // Request account access if needed
+                await ethereum.enable();
+
+                // Accounts now exposed
+                web3.eth.sendTransaction({/* ... --------------/});
+
+            } catch (error) {
+                // User denied account access...
+            }
+
+        }
+
+        else if (window.web3) {
+            window.web3 = new Web3(provider);
+
+            // Accounts always exposed
+            web3.eth.sendTransaction({/* ... --------------/});
+        }
+
+        // Non-dapp browsers...
+        else{
+            console.log('Non-Ethereum browser detected. You should consider MetaMask!!')
+        }
+    },
+
+    loadAccount: async () => {
+        App.account = await ethereum.request({ method: 'eth_accounts' })[0] //eth object will hold all accounts, eth created/loaded by web3
+        console.log(App.account)
+    }
+}
+
+//load the app whenever the project loads..
+$(() => {
+    $(window).load(() => {
+        App.load()
+    })
+
+})
+
+
+
+*/
